708cfd5296a1d54cf5f9d3544c5fee1b5ee923bb
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2020-11-11 18:06:44 +0100

    Johan cimmit

--- Binary Tree/src/com/company/Main.java
+++ Binary Tree/src/com/company/Main.java
@@ -2,23 +2,11 @@ package com.company;
 
 public class Main {
 
-    private Node root;
-
-    private static class Node {
-        Node left;
-        Node right;
-        int key;
-
-        Node(int key) {
-            this.key = key;
-        }
-    }
-
     public static void main(String[] args) {
         int[] test = {5, 3, 789, 1235, 2, 1, 3, 4, 23, 43};
         BubbleSort sortAndPrint = new BubbleSort();
         sortAndPrint.bubbleSort(test);
-        Main tree = new Main();
+        BinaryTree tree = new BinaryTree();
 
         tree.insert(10);
         tree.insert(2);
@@ -33,71 +21,8 @@ public class Main {
         tree.printTreeInOrder();
 
         System.out.print("\n");
-        
-        tree.remove(2);
-        tree.remove(16);
+
+        tree.remove(4);
         tree.printTreeInOrder();
-    }
-
-    private void insert(int key) {
-        root = insertRecursive(root, key);
-    }
-
-    private Node insertRecursive(Node tree, int key) {
-        if (tree == null) {
-            tree = new Node(key);
-            return tree;
-        }
-
-        if (key < tree.key) {
-            tree.left = insertRecursive(tree.left, key);
-        } else if (key > tree.key) {
-            tree.right = insertRecursive(tree.right, key);
-        }
-        return tree;
-    }
-
-    private void remove(int key) {
-        removeRecursive(root, null, key);
-    }
-
-    private void removeRecursive(Node tree, Node parentTree, int keyToRemove) {
-        if (keyToRemove < tree.key) {
-            if (tree.left != null) {
-                removeRecursive(tree.left, tree, keyToRemove);
-            } else {
-                parentTree.left = tree;
-            }
-        } else {
-            if (tree.right != null) {
-                removeRecursive(tree.right, tree, keyToRemove);
-            }
-        }
-    }
-
-    private void printTreeInOrder() {
-        printTreeInOrder(root);
-    }
-
-    private void printTreeInOrder(Node tree) {
-        if (tree == null) {
-            return;
-        }
-        printTreeInOrder(tree.left);
-        System.out.println(tree.key);
-        printTreeInOrder(tree.right);
-    }
-
-    private void printTreeInReverseOrder() {
-        printTreeInReverseOrder(root);
-    }
-
-    private void printTreeInReverseOrder(Node tree) {
-        if (tree == null) {
-            return;
-        }
-        printTreeInReverseOrder(tree.right);
-        System.out.println(tree.key);
-        printTreeInReverseOrder(tree.left);
     }
 }
